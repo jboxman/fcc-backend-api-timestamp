35bed8672fdbb70c59676818a2a07cded5d14891
Jason Boxman <jasonb@edseek.com>
2016-09-21 23:32:57 -0400

    Add build.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./build/backend.js
---
+++ build/backend.js
@@ -0,0 +1,156 @@
+require("source-map-support").install();
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmory imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmory exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		Object.defineProperty(exports, name, {
+/******/ 			configurable: false,
+/******/ 			enumerable: true,
+/******/ 			get: getter
+/******/ 		});
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+module.exports = require("express");
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+'use strict';
+
+var express = __webpack_require__(0);
+var router = express.Router();
+
+router.use('/time', __webpack_require__(3));
+
+/*
+router.get('/', function(req, res) {
+  res.render('index')
+})
+*/
+
+module.exports = router;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+module.exports = require("chrono-node");
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+'use strict';
+
+var express = __webpack_require__(0);
+var router = express.Router();
+
+router.get('/time/:value', function (req, res) {
+  console.log('hi');
+  res.type('text/plain');
+  res.send(req.param['value']);
+});
+
+module.exports = router;
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+'use strict';
+
+var express = __webpack_require__(0);
+var chrono = __webpack_require__(2);
+
+var app = express();
+var port = 3100;
+
+app.use(__webpack_require__(1));
+
+app.get('/', function (req, res) {
+  res.type('text/plain');
+  res.send('Time.');
+});
+
+app.get('/:value', function (req, res) {
+  console.log(req.params['value']);
+  res.type('text/plain');
+  //res.send('Meadowlark Travel');
+  res.json({});
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).render('error');
+});
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=backend.js.map
